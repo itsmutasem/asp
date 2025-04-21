26956ee5e6a59a6ef46ae5db1a619803dce7357e
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-21 17:07:17 +0300

    Add Users Controller

---
+++ WebApplication/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers;
+
+public class UsersController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
