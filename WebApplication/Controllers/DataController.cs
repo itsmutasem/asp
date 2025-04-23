33a62d9d173f0b0949bd048ef44b32332a7e2faf
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-23 14:41:43 +0300

    Add _student in DataController

--- WebApplication/Controllers/DataController.cs
+++ WebApplication/Controllers/DataController.cs
@@ -6,8 +6,14 @@ namespace WebApplication.Controllers;
 public class DataController : Controller
 {
     private List<Course> _courses;
+    private Student _student;
     public DataController()
     {
+        _student = new Student();
+        _student.Id = 1;
+        _student.Name = "Qusai";
+        _student.GPA = 3.76;
+        
         _courses = new List<Course>();
         _courses.Add(new Course() { Id = 1, Name = "PHP" });
         _courses.Add(new Course() { Id = 2, Name = "JS" });
--- WebApplication/Models/Student.cs
+++ WebApplication/Models/Student.cs
@@ -4,5 +4,5 @@ public class Student
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string GPA { get; set; }
+    public double GPA { get; set; }
 }
