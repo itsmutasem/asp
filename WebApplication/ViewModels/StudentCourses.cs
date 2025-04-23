d90597ccb5682c2917227279286889d8a712ff2f
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-23 14:49:27 +0300

    define Student Object in VM StudentCourses

---
+++ WebApplication/ViewModels/StudentCourses.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication.Models;
+
+namespace WebApplication.ViewModels;
+
+public class StudentCourses
+{
+    public Student StudentDetails { get; set; }
+    public List<Course> Courses { get; set; }
+}
