55442b4a5ff063625b97151235ecfa5e9287875e
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-25 18:24:47 +0300

    Add Products List and View

--- WebApplication/Controllers/ProductsController.cs
+++ WebApplication/Controllers/ProductsController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers;
 
 public class ProductsController : Controller
 {
-    // GET
+    private List<Product> _peoducts;
+
+    public ProductsController()
+    {
+        _peoducts = new List<Product>();
+        _peoducts.Add(new Product { Id = 1, Name = "Keyboard", Description = "Black Keyboard", Price = 15.99 });
+        _peoducts.Add(new Product { Id = 2, Name = "Mouse", Description = "Black Mouse", Price = 10.99 });
+        _peoducts.Add(new Product { Id = 3, Name = "Screen", Description = "Wide Screen", Price = 35.99 });
+    }
     public IActionResult Index()
     {
-        return View();
+        return View(_peoducts);
     }
 }
