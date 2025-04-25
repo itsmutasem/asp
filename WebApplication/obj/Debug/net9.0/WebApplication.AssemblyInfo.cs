c36304e96604b04d5a149a20ac3fbc822bf9bb37
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-25 18:31:06 +0300

    Display Products in the View

--- WebApplication/obj/Debug/net9.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Debug/net9.0/WebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a416181ec245ce1f1a19fe00d42bc93d61332e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55442b4a5ff063625b97151235ecfa5e9287875e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
