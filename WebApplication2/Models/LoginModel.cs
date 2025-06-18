57b5423ee5f51155e3736db80de6704672dc8ae6
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-18 23:19:18 +0300

    Define Data in LoginModel

--- WebApplication2/Models/LoginModel.cs
+++ WebApplication2/Models/LoginModel.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApplication2.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models;
 
 public class LoginModel
 {
-    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 }
