4bb0fb037a7b1fbfce17ef2a32f91df80d676578
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-18 23:44:26 +0300

    Default Route to Login Page

--- WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df30850d74890f4bde85aba518c1cc47b142ea6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18893bb12ec7013a99f473003d913ae37e4da26")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
